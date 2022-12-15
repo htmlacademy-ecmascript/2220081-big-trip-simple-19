ba64ef4c2584b27a8becd308e40edf67bdc7b139
Denis <dezzimen1902@gmail.com>
2022-12-15 22:24:09 +0300

    Время разбивать камни

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import TripPresenter from './presenter.js';
+const headerElement = document.querySelector('.trip-controls');
+const tripEventsElement = document.querySelector('.trip-events');
+const tripPresenter = new TripPresenter({
+  boardContainer: tripEventsElement,
+  filterContainer: headerElement,
+});
+
+tripPresenter.init();
